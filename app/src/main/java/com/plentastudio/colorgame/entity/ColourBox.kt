ae3b0d953904ed6c5311477b9fafc98bead1993b
VictorOluwaseun <7ab72599f7abbd8fc3830055530ba7a2c3c081e5>
2021-10-09 01:45:58 +0100

    Updated ColourBox to take rgb

--- app/src/main/java/com/plentastudio/colorgame/entity/ColourBox.kt
+++ app/src/main/java/com/plentastudio/colorgame/entity/ColourBox.kt
@@ -3,5 +3,8 @@ package com.plentastudio.colorgame.entity
 data class ColourBox(
     var id: Long = 0L,
     var color: String? = null,
+    var r: String,
+    var g: String,
+    var b: String,
     var isCorrect: Boolean = false
 )
