d584059fb61ee8b23b497e6d318cf6c472c77ad3
VictorOluwaseun <7ab72599f7abbd8fc3830055530ba7a2c3c081e5>
2021-10-09 02:15:15 +0100

    Completed Recycler view impl

--- app/src/main/java/com/plentastudio/colorgame/adapter/ColourGameAdapter.kt
+++ app/src/main/java/com/plentastudio/colorgame/adapter/ColourGameAdapter.kt
@@ -1,51 +1,94 @@
 package com.plentastudio.colorgame.adapter
 
+import android.graphics.Color
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
-import com.plentastudio.colorgame.R
+import com.plentastudio.colorgame.databinding.HeaderColorGameBinding
+import com.plentastudio.colorgame.databinding.ListItemColorGameBinding
 import com.plentastudio.colorgame.entity.ColourBox
 import com.plentastudio.colorgame.pojo.DataItem
 
 
-private const val  ITEM_VIEW_TYPE_HEADER = 0
+private const val ITEM_VIEW_TYPE_HEADER = 0
 private const val ITEM_VIEW_TYPE_ITEM = 1
 
-class ColourGameAdapter(val clickListener: ColorGameListener): ListAdapter<DataItem, RecyclerView.ViewHolder>(ColourDiffCallback()) {
+class ColourGameAdapter(val clickListener: ColorGameListener) :
+    ListAdapter<DataItem, RecyclerView.ViewHolder>(ColourDiffCallback()) {
+    private val TAG = ColourGameAdapter::class.java.simpleName
+
+    fun addHeaderAndColorItem(list: List<ColourBox>?) {
+        val items = when (list) {
+            null -> listOf(DataItem.Header)
+            else -> listOf(DataItem.Header) + list.map { DataItem.ColorBoxItem(it) }
+        }
+    }
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        return when (viewType) {
+            ITEM_VIEW_TYPE_HEADER -> ColorHeaderViewHolder.from(parent)
+            ITEM_VIEW_TYPE_ITEM -> ColorItemViewHolder.from(parent)
+            else -> throw ClassCastException("Unknown viewType $viewType")
+        }
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        when (holder) {
+            is ColorItemViewHolder -> {
+                val colourItem = getItem(position) as DataItem.ColorBoxItem
+                holder.bind(colourItem.colourBox, clickListener)
+            }
+        }
     }
 
-    class ColorHeaderViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    override fun getItemViewType(position: Int): Int {
+        return when (getItem(position)) {
+            is DataItem.Header -> ITEM_VIEW_TYPE_HEADER
+            is DataItem.ColorBoxItem -> ITEM_VIEW_TYPE_ITEM
+        }
+    }
+
+    class ColorHeaderViewHolder private constructor(val binding: HeaderColorGameBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+
+        fun bind() {
+            binding.clHeader
+            binding.btnNewColours
+            binding.btnEasy
+            binding.btnHard
+        }
+
         companion object {
             fun from(parent: ViewGroup): ColorHeaderViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.header_color_game, parent, false)
+                val view = HeaderColorGameBinding.inflate(layoutInflater, parent, false)
                 return ColorHeaderViewHolder(view)
             }
         }
     }
 
-    class ColorItemViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    class ColorItemViewHolder private constructor(val binding: ListItemColorGameBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(color: ColourBox, clickListener: ColorGameListener) {
+            binding.btnColorBox.setColorFilter(Color.rgb(color.r, color.g, color.b))
+            binding.clickListener = clickListener
+            binding.executePendingBindings()
+        }
+
         companion object {
             fun from(parent: ViewGroup): ColorItemViewHolder {
                 val layoutInflater = LayoutInflater.from(parent.context)
-                val view = layoutInflater.inflate(R.layout.list_item_color_game, parent, false)
-               return ColorItemViewHolder(view)
+                val view = ListItemColorGameBinding.inflate(layoutInflater, parent, false)
+                return ColorItemViewHolder(view)
             }
         }
     }
 
 }
 
-class ColourDiffCallback: DiffUtil.ItemCallback<DataItem>(){
+class ColourDiffCallback : DiffUtil.ItemCallback<DataItem>() {
     override fun areItemsTheSame(oldItem: DataItem, newItem: DataItem): Boolean {
         return oldItem.id == newItem.id
     }
--- app/src/main/java/com/plentastudio/colorgame/entity/ColourBox.kt
+++ app/src/main/java/com/plentastudio/colorgame/entity/ColourBox.kt
@@ -3,8 +3,8 @@ package com.plentastudio.colorgame.entity
 data class ColourBox(
     var id: Long = 0L,
     var color: String? = null,
-    var r: String,
-    var g: String,
-    var b: String,
+    var r: Int,
+    var g: Int,
+    var b: Int,
     var isCorrect: Boolean = false
 )
